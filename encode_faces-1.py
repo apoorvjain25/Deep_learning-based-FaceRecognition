ffdd6c4ef6951ca2823442ddcd2deaac3d971a2d
Apoorv Jain <apoorv.jain25@gmail.com>
2019-04-07 00:18:31 +0530

    Add files via upload

---
+++ encode_faces-1.py
@@ -0,0 +1,108 @@
+# python encode_faces.py --dataset dataset --encodings encodings.pickle
+
+#please add dataset folder path above, which ever you want to use 
+#in the form of different folders for each person with folder name as person name. 
+
+from imutils import paths
+
+import face_recognition
+
+import argparse
+
+import pickle
+
+import cv2
+
+import os
+
+
+import numpy as np
+import pandas as pd 
+
+
+ap = argparse.ArgumentParser()
+
+ap.add_argument("-i", "--dataset", required=True,
+ help="path to input directory of faces + images")
+
+ap.add_argument("-e", "--encodings", required=True,
+ help="path to serialized db of facial encodings")
+
+ap.add_argument("-d", "--detection-method", type=str, default="cnn",
+ help="face detection model to use: either `hog` or `cnn`")
+
+args = vars(ap.parse_args())
+
+  
+
+
+print("[INFO] quantifying faces...")
+
+imagePaths = list(paths.list_images(args["dataset"]))
+
+
+
+
+knownEncodings = []
+
+knownNames = []
+
+
+
+for (i, imagePath) in enumerate(imagePaths):
+	
+	# extract the person name from the image path
+	
+	print("[INFO] processing image {}/{}".format(i + 1,
+ len(imagePaths)))
+	
+	name = imagePath.split(os.path.sep)[-2]
+
+	
+
+	
+	image = cv2.imread(imagePath)
+	
+	rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+	
+	# detect the (x, y)-coordinates of the bounding boxes
+	
+ 	# corresponding to each face in the input image
+	
+	boxes = face_recognition.face_locations(rgb,
+model=args["detection_method"])
+
+	
+	# compute the facial embedding for the face
+	
+	encodings = face_recognition.face_encodings(rgb, boxes)
+
+	
+	
+	for encoding in encodings:
+				
+		knownEncodings.append(encoding)
+		
+		knownNames.append(name)
+
+
+
+
+
+print("[INFO] serializing encodings...")
+
+data = {"encodings": knownEncodings, "names": knownNames}
+
+
+df = pd.DataFrame(knownEncodings)
+df.to_csv("C:\\Python27\\feature.csv")
+
+df1 = pd.DataFrame(knownNames)
+df1.to_csv("C:\\Python27\\Name.csv")
+
+f = open(args["encodings"], "wb")
+
+f.write(pickle.dumps(data))
+
+f.close()
---
+++ recognize_faces_vid.py
@@ -0,0 +1,210 @@
+# python recognize_faces_video_file.py --encodings encodings.pickle --input videos/lunch_scene.mp4
+
+
+
+#python C:\Python27\Test\deep_learning\face-recognition-opencv\recognize_faces_vid.py --encodings Test\deep_learning\face-recognition-opencv\encodings.pickle --input Advait\lunch_scene.mp4 -d 'hog'
+
+import face_recognition
+
+import argparse
+
+import imutils
+
+import pickle
+
+import time
+
+import cv2
+import datetime
+
+
+from time import gmtime, strftime
+showdate = strftime("%Y-%m-%d", gmtime())
+showtime = strftime("%H:%M:%S", gmtime())
+
+
+
+#
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-e", "--encodings", required=True,
+ help="path to serialized db of facial encodings")
+
+ap.add_argument("-i", "--input", required=True,
+	help="path to input video")
+
+ap.add_argument("-o", "--output", type=str,
+ help="path to output video")
+
+ap.add_argument("-y", "--display", type=int, default=1,
+	help="whether or not to display output frame to screen")
+
+ap.add_argument("-d", "--detection-method", type=str, default="cnn",
+ help="face detection model to use: either `hog` or `cnn`")
+
+args = vars(ap.parse_args())
+
+
+
+# load the known faces and embeddings
+
+print("[INFO] loading encodings...")
+
+data = pickle.loads(open(args["encodings"], "rb").read())
+
+
+
+print("[INFO] processing video...")
+
+stream = cv2.VideoCapture(args["input"])
+
+writer = None
+
+
+
+
+while True:
+
+	
+	(grabbed, frame) = stream.read()
+
+
+
+	if not grabbed:
+
+		break
+
+
+	# convert the input frame from BGR to RGB then resize it to have
+
+
+	rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+	rgb = imutils.resize(frame, width=750)
+
+	r = frame.shape[1] / float(rgb.shape[1])
+
+
+
+
+	boxes = face_recognition.face_locations(rgb,
+ model=args["detection_method"])
+
+	encodings = face_recognition.face_encodings(rgb, boxes)
+
+	names = []
+
+
+
+	# loop over the facial embeddings
+
+	for encoding in encodings:
+
+		matches = face_recognition.compare_faces(data["encodings"],
+ encoding)
+
+		name = "Unknown"
+
+
+
+		# check to see if we have found a match
+
+		if True in matches:
+
+
+			matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+
+			counts = {}
+
+
+
+			for i in matchedIdxs:
+
+				name = data["names"][i]
+
+				counts[name] = counts.get(name, 0) + 1
+
+
+			name = max(counts, key=counts.get)
+
+		names.append(name)
+
+
+
+	# loop over the recognized faces
+
+	for ((top, right, bottom, left), name) in zip(boxes, names):
+
+		# rescale the face coordinates
+
+		top = int(top * r)
+
+		right = int(right * r)
+
+		bottom = int(bottom * r)
+
+		left = int(left * r)
+
+
+		
+		# draw the predicted face name on the image
+
+		cv2.rectangle(frame, (left, top), (right, bottom),
+ (0, 255, 0), 2)
+
+		y = top - 15 if top - 15 > 15 else top + 15
+
+		x = bottom - 30
+		z = bottom - 45
+		#bottom - 30 if bottom - 30 > 30 else
+		cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
+ 0.75, (0, 255, 0), 2)
+
+
+		cv2.putText(frame, showtime, (bottom, x), cv2.FONT_HERSHEY_SIMPLEX,
+ 0.75, (0, 255, 0), 2)
+		cv2.putText(frame, showdate, (z, bottom), cv2.FONT_HERSHEY_SIMPLEX,
+ 0.75, (0, 255, 0), 2)
+	
+	if writer is None and args["output"] is not None:
+
+		fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+
+		writer = cv2.VideoWriter(args["output"], fourcc, 24,
+ (frame.shape[1], frame.shape[0]), True)
+
+
+
+	if writer is not None:
+
+		writer.write(frame)
+
+
+
+	if args["display"] > 0:
+
+		cv2.imshow("Frame", frame)
+
+		key = cv2.waitKey(1) & 0xFF
+
+
+
+		# if the `q` key was pressed, break from the loop
+
+		if key == ord("q"):
+
+			break
+
+
+
+# close the video file pointers
+
+stream.release()
+
+
+# check to see if the video writer point needs to be released
+
+if writer is not None:
+
+	writer.release()
+
